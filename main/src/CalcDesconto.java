b3fd908e62a4398d7a6e5f7b36c3511fcf5b6e7c
Thiago <priotothiago@gmail.com>
2025-08-12 19:15:00 -0300

    Sist. Calculo de desconto

--- main/src/CalcDesconto.java
+++ main/src/CalcDesconto.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class CalcDesconto {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        double preco, desconto, ValorFinal = 0.0;
+
+        System.out.println("Digite o nome do produto: ");
+        String nome = sc.nextLine();
+
+        while (true) {
+            try {
+                System.out.println("Digite o Preço do produto: ");
+                preco = sc.nextDouble();
+
+                System.out.println("Desconto do produto: ");
+                desconto = sc.nextDouble();
+
+                ValorFinal = preco - (preco * (desconto / 100));
+
+                break;
+            }
+            catch (InputMismatchException e){
+                System.out.println("Erro! digite apenas números...");
+                sc.nextLine();
+            }
+        }
+        System.out.println("\n---Resumo---");
+        System.out.printf("Produto: %s%n", nome);
+        System.out.printf("Preço original: R$ %.2f%n", preco);
+        System.out.printf("Desconto: %.0f%%%n", desconto);
+        System.out.printf("Preço final: R$ %.2f%n", ValorFinal);
+
+
+        sc.close();
+    }
 }
--- main/src/Lista.java
+++ main/src/Lista.java
@@ -1,6 +1,21 @@
-public class rascunho {
+public class Lista {
     int id;
     int idade;
     String nome;
-    
+
+    public Lista(int id,int idade,String nome){
+        this.id = 222;
+        this.nome = "Luiz";
+        this.idade = 34567;
+    }
+    public void exibir(){
+        System.out.println("ID:" + id);
+        System.out.println("Nome:" + nome);
+        System.out.println("Idade:" + idade);
+    }
+    public static void main(String[] args){
+        Lista Lista = new Lista(222, 34567, "Luiz");
+
+        Lista.exibir();
+    }
 }
